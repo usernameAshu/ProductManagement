6e01cbfb26e4f4e61e124a63f484b298c7c5991f
usernameAshu <mohantyashu.07@gmail.com>
2021-03-31 23:43:40 +0530

    static context , instance context

---
+++ src/labs/pm/test/compostionDesignPattern/Test2.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021, Ashutosh Mohanty
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package labs.pm.test.compostionDesignPattern;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Order.createShippingMode("Fast Delivery");
+//        Order order1 = new Order();
+//        Order.createShippingMode("Slow Delivery");
+    }
+}
+
+class Order {
+
+    public static void createShippingMode(String desciption) {
+        ShippingMode shippingMode = new ShippingMode(desciption);
+        ShippingMode.displayShippingMethod();
+        shippingMode.displayMethod();
+        ShippingMode.description = "Prime Delivery";
+        shippingMode.displayMethod();
+        new ShippingMode("Express Delivery");
+        ShippingMode.displayShippingMethod();
+        shippingMode.displayMethod();
+        System.out.println("------------");
+    }
+
+    private static class ShippingMode {
+        private static String description;
+
+        public ShippingMode(String description) {
+            ShippingMode.description = description;
+        }
+        private void businessLogic() {
+            //
+        }
+        public static void displayShippingMethod() {
+            System.out.println("Static method:" + ShippingMode.description);
+        }
+        public void displayMethod() {
+            System.out.println("Instance method:"+ ShippingMode.description);
+        }
+    }
+}
